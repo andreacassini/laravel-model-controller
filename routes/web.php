f68086ad761b745d51f0f9e048bd8e42c58faa4b
andreacassini <andrea1316@hotmail.it>
2023-07-27 00:41:44 +0200

    first commit [ADD] controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Guest\PageController as PageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {  
     return view('welcome');
-});
+}); */
+
+
+//ROUTE CON CONTROLLER
+Route::get('/', [PageController::class, 'index']);
