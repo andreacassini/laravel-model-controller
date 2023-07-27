d18fac82249f4741c621c246bc0040e7b80c5f87
andreacassini <andrea1316@hotmail.it>
2023-07-27 13:49:11 +0200

    ADD comics from db working

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Comics;
+
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $comics = comics::all();
+        dd($comics);
+        return view('home', compact('comics'));
     }
 }
